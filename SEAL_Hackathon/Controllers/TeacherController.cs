a2d9bc9eb897a161db78bc24d90a72ce62ab79e5
TaiXN <thientai1109@gmail.com>
2026-06-20 00:09:18 +0700

    Update Teacher

--- SEAL_Hackathon/Controllers/TeacherController.cs
+++ SEAL_Hackathon/Controllers/TeacherController.cs
@@ -1,4 +1,6 @@
 ﻿using APIViewModels.Teacher;
+using DataAccess.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Services.AccountService;
@@ -23,6 +25,7 @@ namespace SEAL_Hackathon.Controllers
 
 
         [HttpPost]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Create(CreateTeacherAPIViewModel info)
         {
             if (ModelState.IsValid)
@@ -52,6 +55,70 @@ namespace SEAL_Hackathon.Controllers
 
             }
             else return BadRequest();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAll()
+        {
+            List<Teacher> result = await _teacher.GetAllAsync();
+            return Ok(result);
+        }
+
+     
+        [HttpGet("{id}")]
+        [Authorize(Roles = "Admin, Teacher")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            Teacher result = await _teacher.GetByIdAsync(id);
+            if (result == null)
+            {
+                return NotFound("Teacher not found");
+            }
+            return Ok(result);
+        }
+
+        
+        [HttpPut("{id}")]
+        [Authorize(Roles = "Admin, Teacher")]
+        public async Task<IActionResult> Update(string id, UpdateTeacherAPIViewModel info)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                bool isUpdated = await _teacher.UpdateAsync(id, new Account()
+                {
+                    Email = info.Email,
+                    FullName = info.FullName,
+                    Address = info.Address,
+                    Phone = info.Phone
+                }, info.IsGuest);
+
+                if (isUpdated)
+                {
+                    return Ok("Update teacher successfully");
+                }
+                else
+                {
+                    return BadRequest("Error while updating teacher (ID not found or duplicate Email)");
+                }
+            }
+            else return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            bool isDeleted = await _teacher.DeleteAsync(id);
+            if (isDeleted)
+            {
+                return Ok("Delete teacher successfully");
+            }
+            else
+            {
+                return BadRequest("Error while deleting teacher (ID not found)");
+            }
         }
     }
 }
